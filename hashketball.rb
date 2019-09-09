e9d3af3b934b3097c5fdd53baff9098d28a2b161
Altaf M Quadri <altafmquadri@gmail.com>
2019-09-09 20:22:28 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -142,7 +142,9 @@ end
 def num_points_scored(player_name)
   game_hash.each do |location, team_data|
     binding.pry
-    team_data[:players].each do |name|
+    team_data[:players]
+    
+    
       binding.pry
     end
   end
